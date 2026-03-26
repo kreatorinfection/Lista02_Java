55a3e103b94aab2a8ed44bba146489d968e2ca05
bryanrs silva <bryanrossoni8@gmail.com>
2026-03-26 13:17:09 -0300

    ex12.java

--- ex12.java
+++ ex12.java
@@ -21,3 +21,32 @@ public class ex12 {
 	       
 	    }
 }
+
+
+// Eu tambémf fiz uma versão, só que com bebê:
+
+package prjLista02;
+
+import java.util.Scanner;
+
+public class ex12 {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Digite a idade: ");
+	        int idade = scanner.nextInt();
+
+	        if (idade == 4 && idade <=13) {
+	            System.out.println("Criança");
+	        }else if (idade <=3) {
+	        	System.out.println("Bebê");
+	        } else if (idade >= 13 && idade <= 17) {
+	            System.out.println("Adolescente");
+	        } else {
+	            System.out.println("Adulto");
+	        }
+
+	       
+	    }
+}
